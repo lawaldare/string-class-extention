defd8e3d3341ac73b196fb8c43fa30a5efaf5ec2
Dare Lawal <lawaldare@gmail.com>
2018-12-19 16:43:31 +0100

    all methods declared

---
+++ stringClass/stringClassSolution.js
@@ -0,0 +1,66 @@
+String.prototype.hasVowels = function() {
+		
+
+	};
+
+String.prototype.isQuestion = function() {
+		
+
+	};
+
+String.prototype.toUpper = function() {
+		
+
+	};
+
+String.prototype.toLower = function() {
+		
+
+	};
+
+String.prototype.ucFirst = function() {
+		
+
+	};
+
+String.prototype.words = function() {
+		
+
+	};
+
+String.prototype.wordCount = function() {
+		
+
+	};
+
+String.prototype.inverseCase = function() {
+		
+	};
+
+String.prototype.alternatingCase = function() {
+		
+
+	};
+
+String.prototype.numberWords = function() {
+		
+
+	};
+
+String.prototype.isDigits = function() {
+		
+
+	};
+
+String.prototype.toCurrency = function() {
+		
+
+	};
+
+String.prototype.fromCurrency = function() {
+		
+
+	};
+
+
+module.exports = String
