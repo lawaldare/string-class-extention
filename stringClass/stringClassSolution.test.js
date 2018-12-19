f495da7c6664f524aaf709928fc4d665a015a273
Dare Lawal <lawaldare@gmail.com>
2018-12-19 17:29:05 +0100

    toUpper() cases added

--- stringClass/stringClassSolution.test.js
+++ stringClass/stringClassSolution.test.js
@@ -16,8 +16,14 @@ require('./StringClassSolution');
    test('A case where question mark comes inside the string', () => {
        expect('H?allo'.isQuestion()).toEqual(false);
    })
-   test('A simple case where Vowel is both upper and lower', () => {
-       expect('Amidso'.hasVowels()).toEqual(true);
+   test('A case sentence strings', () => {
+       expect('I miss you'.toUpper()).toEqual('I MISS YOU');
+   })
+   test('A case sentence where all are uppercase', () => {
+       expect('JOSHUA'.toUpper()).toEqual('JOSHUA');
+   })
+   test('A case where uppercase are in between a string', () => {
+       expect('nOPe'.toUpper()).toEqual('NOPE');
    })
 
 
