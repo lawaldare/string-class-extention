cb402f29a3a11faa5c8db8c1a41a5ec2049e81b1
Dare Lawal <lawaldare@gmail.com>
2018-12-19 17:22:26 +0100

    isQuestion() cases added

--- stringClass/stringClassSolution.test.js
+++ stringClass/stringClassSolution.test.js
@@ -6,3 +6,19 @@ require('./StringClassSolution');
    test('A simple case where Vowel is both upper and lower', () => {
        expect('Amidso'.hasVowels()).toEqual(true);
    })
+
+   test('A case where question mark ends the question', () => {
+       expect('Is the world yours, on a blimp?'.isQuestion()).toEqual(true);
+   })
+   test('A simple case with no question mark', () => {
+       expect('What\'s your name'.isQuestion()).toEqual(false);
+   })
+   test('A case where question mark comes inside the string', () => {
+       expect('H?allo'.isQuestion()).toEqual(false);
+   })
+   test('A simple case where Vowel is both upper and lower', () => {
+       expect('Amidso'.hasVowels()).toEqual(true);
+   })
+
+
+
